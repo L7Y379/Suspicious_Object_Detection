18d3cc49065f1c37a69fe2efc2920179dc57253b
L7Y379 <1102907282@qq.com>
2021-04-21 23:03:07 +0800

    Update fido

--- fiDo/test.py
+++ fiDo/test.py
@@ -1,4 +1,17 @@
-import time
+import numpy as np
+l = [3, 2, 1, 0, 4, 5]
 
-localtime = time.asctime( time.localtime(time.time()) )
-print ("本地时间为 :", localtime)
+idx1 = np.array([j for j in range(0 ,3, 1)])
+idx2=np.array([j for j in range(4 ,5, 1)])
+idx=np.hstack((idx1,idx2))
+print(idx)
+l.index(max(l))  # 返回list最大值位置
+#idx = np.array([4,2])
+
+
+array_l = np.array(l)
+print(array_l[idx])
+a=np.argmax(array_l)  # 返回array最大值位置
+idx=np.array([j for j in range(0 ,a, 1)])
+print(array_l[idx,])
+print(a)
